609cff4a2151c572de9bde81e8a4a82065df8c1d
Lovro Posarić <lovro.posaric@gmail.com>
2022-12-20 13:05:11 +0100

    Podaci

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const dbConfig = {
     authentication: {
       options: {
-        userName: "lposaric", // TODO: update
-        password: "Lovro-69" // TODO: update
+        userName: "", // TODO: update
+        password: "" // TODO: update
       },
       type: "default"
     },
-    server: "rus22lposaric.database.windows.net", // TODO: update
+    server: "+net", // TODO: update
     options: {
       database: "BuildingData", //TODO: update
       encrypt: true
